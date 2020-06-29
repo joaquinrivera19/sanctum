3526f4bfb032b80e2e933888350a44ab81ef3117
Rivera Joaquin <joaquinrivera657@gmail.com>
2020-06-29 20:38:42 -0300

    segundo commit

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+
+class UserController extends Controller
+{
+    function Index(Request $request)
+    {
+
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+    
+        $user= User::where('email', $request->email)->first();
+        // print_r($data);
+        
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            return response([
+                'message' => ['These credentials do not match our records.']
+            ], 404);
+        }
+        
+        $token = $user->createToken('my-app-token')->plainTextToken;
+        
+        $response = [
+            'user' => $user,
+            'token' => $token
+        ];
+        
+        return response($response, 201);
+
+    }
+
+    function Users()
+    {
+        return User::all();
+    }
+}
---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\DB;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => 'Joaquin Rivera',
+            'email' => 'joaquin@rivera.com',
+            'password' => Hash::make('1234')
+        ]);
+    }
+}
